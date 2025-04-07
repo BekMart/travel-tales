25f868d48db1fa87cdc8983caa73aff517f43464
BekMart <bekmart1989@gmail.com>
2025-04-07 19:35:00 +0100

    Create navbar component with basic styling

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,11 +3,6 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true,
-//   headers: {
-//     'Content-Type': 'miultipart/form-data',
-//   }
 });
 
 export default api;
-export const axiosReq = axios.create();
-export const axiosRes = axios.create();
