fa6acfffe84a57ba34b65364a94123a742d14f4c
BekMart <bekmart1989@gmail.com>
2025-04-14 13:11:47 +0100

    Create basic log in form and link to api

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import { Container } from "react-bootstrap";
 import { Route, Switch } from "react-router-dom";
-import api from "./api/axios";
 import SignUpForm from "./components/pages/auth/SignUpForm";
+import SignInForm from "./components/pages/auth/SignInForm";
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
           <Route exact path="/" render={() => <h1>Home</h1>} />
           <Route exact path="/profile" render={() => <h1>Profile</h1>} />
           <Route exact path="/feed" render={() => <h1>Feed</h1>} />
-          <Route exact path="/login" render={() => <h1>Login</h1>} />
+          <Route exact path="/login" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route render={() => <h1>404 Not Found</h1>} />
         </Switch>
---
+++ src/components/pages/auth/SignInForm.js
@@ -0,0 +1,131 @@
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import styles from "../../../styles/SignInUpForm.module.css";
+import btnStyles from "../../../styles/Button.module.css";
+import appStyles from "../../../App.module.css";
+
+import {
+  Form,
+  Button,
+  Image,
+  Col,
+  Row,
+  Container,
+  Alert,
+} from "react-bootstrap";
+import api from "../../../api/axios";
+
+const SignInForm = () => {
+  const [signInData, setSignInData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const { username, password } = signInData;
+
+  const [error, setError] = useState({});
+
+  const history = useHistory();
+
+  const handleChange = (e) => {
+    setSignInData({
+      ...signInData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await api.post("/dj-rest-auth/login/", signInData);
+      history.push("/");
+    } catch (error) {
+      setError(error.response?.data);
+    }
+  };
+
+  return (
+    <Row className={styles.Row}>
+      <Col className="my-auto py-2 p-md-2" md={6}>
+        <Container className={`${appStyles.Content} p-4`}>
+          <h1 className={styles.Header}>Log in</h1>
+
+          <Form onSubmit={handleSubmit}>
+
+            {/* Username */}
+            {error.username ? (
+              error.username?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                  {message}
+                </Alert>
+              ))
+            ) : (
+              <Form.Group controlId="username">
+                <Form.Control
+                  className={styles.Input}
+                  type="text"
+                  placeholder="Username"
+                  name="username"
+                  value={username}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+            )}
+
+            {/* Password */}
+            {error.password ? (
+              error.password?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                  {message}
+                </Alert>
+              ))
+            ) : (
+              <Form.Group controlId="password">
+                <Form.Control
+                  className={styles.Input}
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={password}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+            )}
+
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.SignIn}`}
+              type="submit"
+            >
+              Log in
+            </Button>
+            {error.non_field_errors?.map((message, idx) => (
+              <Alert key={idx} variant="warning" className="mt-3">
+                {message}
+              </Alert>
+            ))}
+          </Form>
+        </Container>
+
+        <Container className={`mt-3 ${appStyles.Content}`}>
+          <Link className={styles.Link} to="/signup">
+            Don't have an account? <span>Sign up now!</span>
+          </Link>
+        </Container>
+      </Col>
+
+      <Col
+        md={6}
+        className={`my-auto d-none d-md-block p-4 ${styles.SignUpCol}`}
+      >
+        <Image
+          className={styles.SignInImage}
+          src="https://res.cloudinary.com/dvgobcuck/image/upload/v1744541812/pexels-andreimike-1271619_aktwma.jpg"
+          alt="Image of a man standing atop of a grassy mountain range that he is hiking and admiring the beautiful view"
+          fluid
+        />
+      </Col>
+    </Row>
+  );
+};
+
+export default SignInForm;
