69d79b53dda026a3851d86be630f54691302b20b
BekMart <bekmart1989@gmail.com>
2025-04-14 11:35:54 +0100

    Add basic form validation

--- src/components/pages/auth/SignUpForm.js
+++ src/components/pages/auth/SignUpForm.js
@@ -1,49 +1,160 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import styles from "../../../styles/SignInUpForm.module.css";
-import btnStyles from "../../../styles/Button.module.css"
+import btnStyles from "../../../styles/Button.module.css";
 import appStyles from "../../../App.module.css";
 
-
-import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+import {
+  Form,
+  Button,
+  Image,
+  Col,
+  Row,
+  Container,
+  Alert,
+} from "react-bootstrap";
+import api from "../../../api/axios";
 
 const SignUpForm = () => {
+  const [signUpData, setSignUpData] = useState({
+    // firstName: "",
+    // lastName: "",
+    username: "",
+    password1: "",
+    password2: "",
+  });
+
+  //   const { firstName, lastName, username, password1, password2 } = signUpData;
+  const { username, password1, password2 } = signUpData;
+
+  const [error, setError] = useState({});
+
+  const history = useHistory();
+
+  const handleChange = (e) => {
+    setSignUpData({
+      ...signUpData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await api.post("/dj-rest-auth/registration/", signUpData);
+      history.push("/login");
+    } catch (error) {
+      setError(error.response?.data);
+    }
+  };
+
   return (
     <Row className={styles.Row}>
       <Col className="my-auto py-2 p-md-2" md={6}>
-        <Container className={`${appStyles.Content} p-4 `}>
-          <h1 className={styles.Header}>sign up</h1>
+        <Container className={`${appStyles.Content} p-4`}>
+          <h1 className={styles.Header}>Sign up</h1>
 
-          <Form>
-            <Form.Group controlId="formBasicId">
-              <Form.Label className="d-none">Name</Form.Label>
-              <Row>
-                <Col>
-                  <Form.Control className={styles.Input} placeholder="First name" />
-                </Col>
-                <Col>
-                  <Form.Control className={styles.Input} placeholder="Last name" />
-                </Col>
-              </Row>
-            </Form.Group>
+          <Form onSubmit={handleSubmit}>
+            {/* First and Last Name */}
+            {/* <Row>
+              <Col>
+                <Form.Group controlId="firstName">
+                  <Form.Control
+                    className={styles.Input}
+                    type="text"
+                    placeholder="First Name"
+                    name="firstName"
+                    value={firstName}
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId="lasstName">
+                  <Form.Control
+                    className={styles.Input}
+                    type="text"
+                    placeholder="Last Name"
+                    name="lastName"
+                    value={lastName}
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+              </Col>
+            </Row> */}
 
-            <Form.Group controlId="username">
-              <Form.Label className="d-none">Username</Form.Label>
-              <Form.Control className={styles.Input} type="text" placeholder="Username" />
-            </Form.Group>
+            {/* Username */}
+            {error.username ? (
+              error.username?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                  {message}
+                </Alert>
+              ))
+            ) : (
+              <Form.Group controlId="username">
+                <Form.Control
+                  className={styles.Input}
+                  type="text"
+                  placeholder="Username"
+                  name="username"
+                  value={username}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+            )}
 
-            <Form.Group controlId="password1">
-              <Form.Label className="d-none">Password</Form.Label>
-              <Form.Control className={styles.Input} type="password" placeholder="Password" name="password1"/>
-            </Form.Group>
-            <Form.Group controlId="password2">
-              <Form.Label className="d-none">Confirm Password</Form.Label>
-              <Form.Control className={styles.Input} type="password" placeholder="Confirm Password" name="passwrod2" />
-            </Form.Group>
-            
-            <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.SignUp}`} variant="secondary" type="submit">
+            {/* Password */}
+            {error.password1 ? (
+              error.password1?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                  {message}
+                </Alert>
+              ))
+            ) : (
+              <Form.Group controlId="password1">
+                <Form.Control
+                  className={styles.Input}
+                  type="password"
+                  placeholder="Password"
+                  name="password1"
+                  value={password1}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+            )}
+
+            {/* Confirm Password */}
+            {error.password2 ? (
+              error.password2?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                  {message}
+                </Alert>
+              ))
+            ) : (
+              <Form.Group controlId="password2">
+                <Form.Control
+                  className={styles.Input}
+                  type="password"
+                  placeholder="Confirm Password"
+                  name="password2"
+                  value={password2}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+            )}
+
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.SignUp}`}
+              variant="secondary"
+              type="submit"
+            >
               Sign up
             </Button>
+            {error.non_field_errors?.map((message, idx) => (
+              <Alert key={idx} variant="warning" className="mt-3">
+                {message}
+              </Alert>
+            ))}
           </Form>
         </Container>
 
@@ -60,9 +171,7 @@ const SignUpForm = () => {
       >
         <Image
           className={styles.SignUpImage}
-          src={
-            "https://res.cloudinary.com/dvgobcuck/image/upload/v1744541805/pexels-ajay-donga-1113836-2174656_rn9utz.jpg"
-          }
+          src="https://res.cloudinary.com/dvgobcuck/image/upload/v1744541805/pexels-ajay-donga-1113836-2174656_rn9utz.jpg"
           alt="Sign Up image of a couple on a yellow motorbike riding on a road with tropical palm trees on either side of the road"
           fluid
         />
