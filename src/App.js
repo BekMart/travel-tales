07983f8e00d0fd29dea3ab5ea9f7b479e984c1b0
BekMart <bekmart1989@gmail.com>
2025-04-16 14:53:49 +0100

    Create basic post page view

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Switch } from "react-router-dom";
 import SignUpForm from "./components/pages/auth/SignUpForm";
 import SignInForm from "./components/pages/auth/SignInForm";
 import PostCreateForm from "./components/pages/posts/PostCreateForm";
+import PostPage from "./components/pages/posts/PostPage";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route exact path="/login" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
+          <Route exact path="/posts/:id" render={() => <PostPage />} />
           <Route render={() => <h1>404 Not Found</h1>} />
         </Switch>
       </Container>
