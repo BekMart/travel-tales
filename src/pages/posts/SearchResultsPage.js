976227650bc8412cf92bbbc3e24f46d53b9b668d
BekMart <bekmart1989@gmail.com>
2025-04-27 18:49:46 +0100

    Amend search page styling and add heading

--- src/pages/posts/SearchResultsPage.js
+++ src/pages/posts/SearchResultsPage.js
@@ -1,72 +1,84 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import { Container, Row, Col } from "react-bootstrap";
+import { Row, Col, Container } from "react-bootstrap";
 import api from "../../api/axios";
 import Post from "./Post";
 import Asset from "../../components/Asset";
-import appStyles from "../../App.module.css";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utils";
+import PopularProfiles from "../profiles/PopularProfiles";
+import PopularLocations from "../locations/PopularLocations";
+import PopularPosts from "./PopularPosts";
 import NoResults from "../../assets/no-results.png";
+import appStyles from "../../App.module.css";
+import styles from "../../styles/Post.module.css";
 
 const useQuery = () => new URLSearchParams(useLocation().search);
 
 const SearchResultsPage = () => {
   const query = useQuery().get("q");
-  const [posts, setPosts] = useState([]);
-//   const [profiles, setProfiles] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [posts, setPosts] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
 
   useEffect(() => {
     const fetchResults = async () => {
-      setLoading(true);
       try {
-        const [postsRes] = await Promise.all([
-          api.get(`/posts/?search=${query}`),
-          api.get(`/profiles/?search=${query}`),
-        ]);
-        setPosts(postsRes.data.results || postsRes.data);
-        // setProfiles(profilesRes.data.results || profilesRes.data);
+        const { data } = await api.get(`/posts/?search=${query}`);
+        setPosts({ results: data.results || [] });
+        setHasLoaded(true);
       } catch (err) {
         console.error("Search error:", err);
-      } finally {
-        setLoading(false);
       }
     };
 
     if (query) {
+      setHasLoaded(false);
       fetchResults();
     } else {
-      setLoading(false);
-      setPosts([]);
-    //   setProfiles([]);
+      setPosts({ results: [] });
+      setHasLoaded(true);
     }
   }, [query]);
 
   return (
-    <Container>
-      <h2 className="my-4">Search Results for "{query}"</h2>
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <h2 className={styles.Header}>Search Results for "{query}"</h2>
 
-      {loading ? (
-        <Container className={appStyles.Content}>
-          <Asset spinner />
-        </Container>
-      ) : (
-        <>
-          {posts.length ? (
-            <Row>
-              {posts.map((post) => (
-                <Col md={6} lg={4} key={post.id}>
-                  <Post {...post} />
-                </Col>
-              ))}
-            </Row>
-          ) : (
-            <Container className={appStyles.Content}>
-              <Asset src={NoResults} message="No results found for that search. Please try again." />
-            </Container>
-          )}
-        </>
-      )}
-    </Container>
+        {hasLoaded ? (
+          <>
+            {posts.results.length ? (
+              <InfiniteScroll
+                children={posts.results.map((post) => (
+                  <Post key={post.id} {...post} setPosts={setPosts} />
+                ))}
+                dataLength={posts.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!posts.next}
+                next={() => fetchMoreData(posts, setPosts)}
+              />
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset
+                  src={NoResults}
+                  message={`No results found for "${query}". Please try again.`}
+                />
+              </Container>
+            )}
+          </>
+        ) : (
+          <Container className={appStyles.Content}>
+            <Asset spinner />
+          </Container>
+        )}
+      </Col>
+
+      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+        <PopularProfiles />
+        <PopularLocations />
+        <PopularPosts />
+      </Col>
+    </Row>
   );
 };
 
