18a46044229ac8c1355f46331ce6e6a129d60ff8
BekMart <bekmart1989@gmail.com>
2025-05-05 19:21:40 +0100

    Migrate to React 18 createRoot API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 import { CurrentUserProvider } from "./contexts/CurrentUserContext";
 import { ProfileDataProvider } from "./contexts/ProfileDataContext";
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
 
 ReactDOM.render(
     <Router>
